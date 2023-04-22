5095d166e9bca0e8a45226e9213c502c2170fdc0
alpertunga-bile <76731692+alpertunga-bile@users.noreply.github.com>
2023-04-22 18:11:37 +0000

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -4,5 +4,5 @@ call py -m venv env &
 call .\env\Scripts\activate &
 call .\env\Scripts\pip.exe install basicsr facexlib gfpgan &
 call .\env\Scripts\pip.exe install -r requirements.txt &
-call .\env\Scripts\python.exe setup.py develop
+call .\env\Scripts\python.exe setup.py develop &
 call deactivate
