2f2b70901de009a973ca0db7470771b8cebbd80b
alpertunga-bile <holy_celestine@hotmail.com>
2026-01-30 23:55:00 +0300

    start script is updated with new venv manager

--- start.py
+++ start.py
@@ -1,33 +1,9 @@
-import os
-from VenvManager import VenvManager
+import venv_manager
 
-from os.path import exists, join
-from os import mkdir, remove
-
-def CheckFolder(foldername : str) -> None:
-    if exists(foldername) is False:
-        mkdir(foldername)
-
-def RemoveIfExists(filename : str) -> None:
-    if exists(filename):
-        remove(filename)
 
 if __name__ == "__main__":
-    venv_manager = VenvManager()
-    realesrgan_repokey = "realesrgan"
+    venv_manager = venv_manager.VenvManager()
 
-    CheckFolder("third-party")
+    venv_manager.install_w_requirements()
 
-    if exists(join("third-party", "Real-ESRGAN")) is False:
-        venv_manager.CloneRepository("https://github.com/xinntao/Real-ESRGAN.git", realesrgan_repokey, "third-party")
-        venv_manager.InstallRequirementsFromRepository(realesrgan_repokey)
-        venv_manager.RunScriptInsideRepository(realesrgan_repokey, "setup.py develop")
-
-    RemoveIfExists("0.2.5")
-    RemoveIfExists("1.3.5")
-    RemoveIfExists("1.4.2")
-    RemoveIfExists("1.7")
-
-    venv_manager.InstallWRequirements()
-
-    venv_manager.RunScript("main")
+    venv_manager.run_script("main")
